e3e7c97f096012ecf82b2b6059d32e2c42e31acd
DHEA-ANANDA\LENOVO <coach.ugenx@gmail.com>
2023-01-19 07:13:54 +0700

    success, menampilkan data

--- controller.js
+++ controller.js
@@ -3,6 +3,17 @@
 let response = require('./res');
 let connection = require('./koneksi');
 
-exports.index = function(req, res) {
-    response.ok("Aplikasi REST API berjalan")
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API berjalan", res)
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
