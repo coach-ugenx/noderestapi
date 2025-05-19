35daadfa60c3e7b300a76e3f3530a83e7c7c5644
Dhea Ananda <coach.ugenx@gmail.com>
2025-05-19 14:58:35 +0800

    berhasil menambahkan data baru metode POST

--- controller.js
+++ controller.js
@@ -1,32 +1,54 @@
-'use strict';
+"use strict";
 
-var response = require('./res');
-var connection = require('./koneksi');
+var response = require("./res");
+var connection = require("./koneksi");
 
-exports.index = function(req, res) {
-    response.ok('Aplikasi REST API berjalan !!!', res)
-}
+exports.index = function (req, res) {
+  response.ok("Aplikasi REST API berjalan !!!", res);
+};
 
 //menampilkan semua datA
-exports.tampilData = function(req, res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
-        if(error) {
-            connection.log(error)
-        } else {
-            response.ok(rows, res)
-        }
-    })
-}
+exports.tampilData = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
 
 //menampilkan data berdasarkan id
-exports.tampilDataID = function(req, res) {
-    let id = req.params.id;
-    connection.query('SELECT * FROM mahasiswa WHERE id_mhs = ?', [id],
-        function(error, rows, fields) {
-        if(error) {
-            connection.log(error)
-        } else {
-            response.ok(rows, res)
-        }
-    })
-}
+exports.tampilDataID = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id_mhs = ?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        connection.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
+
+//menambahkan data
+exports.tambahData = function (req, res) {
+  var nama = req.body.nama;
+  var nim = req.body.nim;
+  var jurusan = req.body.jurusan;
+
+  connection.query(
+    "INSERT INTO mahasiswa (nama,nim,jurusan) VALUES(?,?,?)",
+    [nama, nim, jurusan],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Data Berhasil Ditambahkan", res);
+      }
+    }
+  );
+};
--- routes.js
+++ routes.js
@@ -1,11 +1,9 @@
-'use strict';
+"use strict";
 
-module.exports = function(app) {
-    var jsonku = require('./controller');
-    app.route('/')
-        .get(jsonku.index);
-    app.route('/GET')
-        .get(jsonku.tampilData);
-    app.route('/GET/:id')
-        .get(jsonku.tampilDataID)
-}
+module.exports = function (app) {
+  var jsonku = require("./controller");
+  app.route("/").get(jsonku.index);
+  app.route("/GET").get(jsonku.tampilData);
+  app.route("/GET/:id").get(jsonku.tampilDataID);
+  app.route("/POST").post(jsonku.tambahData);
+};
